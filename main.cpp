b6699b4c3ca741b72225a31fa72b094c6638c9d7
meisa233 <39454391+meisa233@users.noreply.github.com>
2024-04-26 17:19:17 +0800

    update code for onnx 1.17

--- main.cpp
+++ main.cpp
@@ -3,8 +3,9 @@
 #include <iostream>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
-//#include <cuda_provider_factory.h>  ///Èç¹ûÊ¹ÓÃcuda¼ÓËÙ£¬ĞèÒªÈ¡Ïû×¢ÊÍ
+#include <cuda_provider_factory.h>  ///å¦‚æœä½¿ç”¨cudaåŠ é€Ÿï¼Œéœ€è¦å–æ¶ˆæ³¨é‡Š
 #include <onnxruntime_cxx_api.h>
+#include <onnxruntime_c_api.h>
 
 using namespace cv;
 using namespace std;
@@ -26,8 +27,8 @@ private:
 	Env env = Env(ORT_LOGGING_LEVEL_ERROR, "Frame Interpolation");
 	Ort::Session *ort_session = nullptr;
 	SessionOptions sessionOptions = SessionOptions();
-	vector<char*> input_names;
-	vector<char*> output_names;
+	vector<string> input_names;
+	vector<string> output_names;
 	vector<vector<int64_t>> input_node_dims; // >=1 outputs
 	vector<vector<int64_t>> output_node_dims; // >=1 outputs
 	Ort::MemoryInfo memory_info_handler = Ort::MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeCPU);
@@ -35,19 +36,35 @@ private:
 
 RIFE::RIFE(string model_path)
 {
-	std::wstring widestr = std::wstring(model_path.begin(), model_path.end());  ////windowsĞ´·¨
-	///OrtStatus* status = OrtSessionOptionsAppendExecutionProvider_CUDA(sessionOptions, 0);   ///Èç¹ûÊ¹ÓÃcuda¼ÓËÙ£¬ĞèÒªÈ¡Ïû×¢ÊÍ
+	///std::wstring widestr = std::wstring(model_path.begin(), model_path.end());  ////windowså†™æ³•
+        OrtCUDAProviderOptions options;
+        options.device_id = 0;
+        options.arena_extend_strategy = 0;
+        options.gpu_mem_limit = 10 * 1024 * 1024 * 1024;
+        options.cudnn_conv_algo_search = OrtCudnnConvAlgoSearchHeuristic;
+        options.do_copy_in_default_stream = 1;
+        options.user_compute_stream = nullptr;
+        options.default_memory_arena_cfg = nullptr;
+	//SessionOptionsAppendExecutionProvider_CUDA(sessionOptions, &options);   ///å¦‚æœä½¿ç”¨cudaåŠ é€Ÿï¼Œéœ€è¦å–æ¶ˆæ³¨é‡Š
+        sessionOptions.AppendExecutionProvider_CUDA(options);
 
-	sessionOptions.SetGraphOptimizationLevel(ORT_ENABLE_BASIC);
-	ort_session = new Session(env, widestr.c_str(), sessionOptions); ////windowsĞ´·¨
-	////ort_session = new Session(env, model_path.c_str(), sessionOptions); ////linuxĞ´·¨
+	//sessionOptions.SetGraphOptimizationLevel(ORT_ENABLE_BASIC);
+        sessionOptions.SetGraphOptimizationLevel(GraphOptimizationLevel::ORT_ENABLE_ALL);
+	///ort_session = new Session(env, widestr.c_str(), sessionOptions); ////windowså†™æ³•
+	ort_session = new Session(env, model_path.c_str(), sessionOptions); ////linuxå†™æ³•
 
 	size_t numInputNodes = ort_session->GetInputCount();
 	size_t numOutputNodes = ort_session->GetOutputCount();
+        for (int i = 0; i < numInputNodes; i++){input_names.push_back(string(""));}
+        for (int i = 0; i < numOutputNodes; i++){output_names.push_back(string(""));}
 	AllocatorWithDefaultOptions allocator;
 	for (int i = 0; i < numInputNodes; i++)
 	{
-		input_names.push_back(ort_session->GetInputName(i, allocator));
+		//input_names.push_back(ort_session->GetInputName(i, allocator));
+                
+                AllocatedStringPtr input_name = ort_session->GetInputNameAllocated(i, allocator);
+                input_names[i].append(input_name.get());
+                 cout<<input_names[i]<<endl;
 		Ort::TypeInfo input_type_info = ort_session->GetInputTypeInfo(i);
 		auto input_tensor_info = input_type_info.GetTensorTypeAndShapeInfo();
 		auto input_dims = input_tensor_info.GetShape();
@@ -55,7 +72,10 @@ RIFE::RIFE(string model_path)
 	}
 	for (int i = 0; i < numOutputNodes; i++)
 	{
-		output_names.push_back(ort_session->GetOutputName(i, allocator));
+		//output_names.push_back(ort_session->GetOutputName(i, allocator));
+                AllocatedStringPtr output_name = ort_session->GetOutputNameAllocated(i, allocator);
+                output_names[i].append(output_name.get());
+                cout<<output_names[i]<<endl;
 		Ort::TypeInfo output_type_info = ort_session->GetOutputTypeInfo(i);
 		auto output_tensor_info = output_type_info.GetTensorTypeAndShapeInfo();
 		auto output_dims = output_tensor_info.GetShape();
@@ -94,16 +114,22 @@ Mat RIFE::interpolate(Mat srcimg1, Mat srcimg2)
 {
 	const int srch = srcimg1.rows;
 	const int srcw = srcimg1.cols;
+        const std::array inputNames = {input_names[0].c_str(), input_names[1].c_str()};
+        const std::array outNames = {output_names[0].c_str()};
 	this->preprocess(srcimg1, this->input1_image);
 	this->preprocess(srcimg2, this->input2_image);
 
+        for(auto item : output_names){std::cout<<"output node:"<<item<<endl;}
 	std::vector<int64_t> input_img_shape = { 1, 3, this->inpHeight, this->inpWidth };
 	std::vector<Ort::Value> inputTensors;
 	inputTensors.push_back((Ort::Value::CreateTensor<float>(memory_info_handler, this->input1_image.data(), this->input1_image.size(), input_img_shape.data(), input_img_shape.size())));
 	inputTensors.push_back((Ort::Value::CreateTensor<float>(memory_info_handler, this->input2_image.data(), this->input2_image.size(), input_img_shape.data(), input_img_shape.size())));
 
+        for(auto item : output_names){std::cout<<"output node:"<<item<<endl;}
 	Ort::RunOptions runOptions;
-	vector<Value> ort_outputs = this->ort_session->Run(runOptions, this->input_names.data(), inputTensors.data(), inputTensors.size(), this->output_names.data(), output_names.size());
+        
+	vector<Value> ort_outputs = this->ort_session->Run(runOptions, inputNames.data(), inputTensors.data(), inputTensors.size(), outNames.data(), outNames.size());
+
 
 	float* pdata = ort_outputs[0].GetTensorMutableData<float>();
 	std::vector<int64_t> outs_shape = ort_outputs[0].GetTensorTypeAndShapeInfo().GetShape();
@@ -116,7 +142,7 @@ Mat RIFE::interpolate(Mat srcimg1, Mat srcimg2)
 	rmat *= 255.f;
 	gmat *= 255.f;
 	bmat *= 255.f;
-	///output_image = µÈ¼Ûnp.clip(output_image, 0, 255)
+	///output_image = ç­‰ä»·np.clip(output_image, 0, 255)
 	rmat.setTo(0, rmat < 0);
 	rmat.setTo(255, rmat > 255);
 	gmat.setTo(0, gmat < 0);
@@ -147,25 +173,34 @@ vector<Mat> RIFE::interpolate_4x(Mat srcimg1, Mat srcimg2)
 
 int main()
 {
+        printf("begin");
 	RIFE mynet("RIFE_HDv3.onnx");
 	const int use_video = 0;
 	if (use_video == 0)
 	{
 		vector<string> inputs_imgpath = { "testimgs/frame07.png", "testimgs/frame08.png", "testimgs/frame09.png", "testimgs/frame10.png", "testimgs/frame11.png", "testimgs/frame12.png", "testimgs/frame13.png", "testimgs/frame14.png" };
+                //vector<string> inputs_imgpath = {"output_1.png", "output_2.png"};
 		const int imgnum = inputs_imgpath.size();
+                printf("test!");
 		if (imgnum < 2)
 		{
 			printf("input must be at least two or more images");
 			exit(1);
 		}
 
+                clock_t start, stop;
+                double count_ =0.0;
 		for (int i = 0; i < imgnum - 1; i++)
 		{
 			Mat srcimg1 = imread(inputs_imgpath[i]);
 			Mat srcimg2 = imread(inputs_imgpath[i + 1]);
+                        start = clock();
 			Mat mid_img = mynet.interpolate(srcimg1, srcimg2);
 
-			string save_imgpath = "imgs_results/mid" + to_string(i) + ".jpg";
+                        stop = clock();
+                        count_ += (double)(stop-start)/CLOCKS_PER_SEC;
+                        cout<<"Use Time:"<<(double)(stop-start)<<endl;
+			string save_imgpath = "mid" + to_string(i) + ".jpg";
 			imwrite(save_imgpath, mid_img);
 
 			/*namedWindow("srcimg1", WINDOW_NORMAL);
@@ -177,6 +212,7 @@ int main()
 			waitKey(0);
 			destroyAllWindows();*/
 		}
+                cout<<count_<<endl;
 	}
 	else
 	{
